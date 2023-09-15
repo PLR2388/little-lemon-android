1d94637b8c10d5b2b242f0fb0ca10d978af65a92
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-15 16:37:11 +0200

    Basic Onboarding screen

---
+++ app/src/main/java/fr/wonderfulappstudio/littlelemon/Onboarding.kt
@@ -0,0 +1,127 @@
+package fr.wonderfulappstudio.littlelemon
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Onboarding() {
+    var firstName by remember {
+        mutableStateOf("")
+    }
+    var lastName by remember {
+        mutableStateOf("")
+    }
+    var email by remember {
+        mutableStateOf("")
+    }
+
+
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = {
+            Image(
+                modifier = Modifier.height(50.dp),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+            )
+        }, modifier = Modifier.fillMaxWidth())
+    }, bottomBar = {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            shape = RoundedCornerShape(4.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF4CE14))
+        ) {
+            Text("Register", color = Color.Black)
+        }
+    }) { padding ->
+        LazyColumn(contentPadding = padding) {
+            item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .background(Color(0xFF496E57)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Let's get to know you",
+                        color = Color.White,
+                        fontSize = 28.sp
+                    )
+                }
+            }
+            item {
+                Column(
+                    verticalArrangement = Arrangement.SpaceAround,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Text("Personal information", fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(32.dp))
+                    OutlinedTextField(
+                        value = firstName,
+                        onValueChange = { firstName = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "First name") }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    OutlinedTextField(
+                        value = lastName,
+                        onValueChange = { lastName = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Last name") }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Email") }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OnboardingPreview() {
+    Onboarding()
+}
